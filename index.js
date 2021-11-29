d847566b96d51a0e2b698e8a729353031b0ecd79
Anthony Riera <anthony@slite.com>
2021-11-28 20:07:37 -0500

    apply changes

--- index.js
+++ index.js
@@ -6,44 +6,56 @@
 // https://github.com/ethereum/web3.js
 /////////////////////////////////////////////////////
 
-const AWS = require('aws-sdk');
-const WebsocketProvider = require('web3-providers-ws');
-const { w3cwebsocket } = require('websocket');
+const AWS = require("aws-sdk");
+const WebsocketProvider = require("web3-providers-ws");
+const { w3cwebsocket } = require("websocket");
 
 const Ws = w3cwebsocket;
 
 module.exports = class AWSWebsocketProvider extends WebsocketProvider {
   constructor(url, options) {
-    super(url, options)
-  } 
+    super(url, options);
+  }
 
   connect() {
-    const region = process.env.AWS_DEFAULT_REGION || 'us-east-1';
+    const region = process.env.AWS_DEFAULT_REGION || "us-east-1";
     const creds =
-      'clientConfig' in this &&
+      "clientConfig" in this &&
       this.clientConfig !== undefined &&
-      'credentials' in this.clientConfig &&
-      'accessKeyId' in this.clientConfig.credentials &&
-      'secretAccessKey' in this.clientConfig.credentials &&
+      "credentials" in this.clientConfig &&
+      "accessKeyId" in this.clientConfig.credentials &&
+      "secretAccessKey" in this.clientConfig.credentials &&
       this.clientConfig.credentials;
-    const credentials = (creds && new AWS.Credentials(creds)) || new AWS.EnvironmentCredentials('AWS') || new AWS.EC2MetadataCredentials();
+    const credentials =
+      (creds && new AWS.Credentials(creds)) ||
+      new AWS.EnvironmentCredentials("AWS");
     const host = new URL(this.url).hostname;
     const endpoint = new AWS.Endpoint(`https://${host}/`);
     const req = new AWS.HttpRequest(endpoint, region);
-    req.method = 'GET';
-    req.body = '';
-    req.headers['host'] = host;
-    const signer = new AWS.Signers.V4(req, 'managedblockchain');
+    req.method = "GET";
+    req.body = "";
+    req.headers["host"] = host;
+    const signer = new AWS.Signers.V4(req, "managedblockchain");
     signer.addAuthorization(credentials, new Date());
     let headers = {
-      'Authorization': req.headers['Authorization'],
-      'X-Amz-Date': req.headers['X-Amz-Date'],
+      Authorization: req.headers["Authorization"],
+      "X-Amz-Date": req.headers["X-Amz-Date"],
       ...this.headers
-    }
+    };
     if (process.env.AWS_SESSION_TOKEN) {
-      headers = { ...headers, 'X-Amz-Security-Token': process.env.AWS_SESSION_TOKEN };
+      headers = {
+        ...headers,
+        "X-Amz-Security-Token": process.env.AWS_SESSION_TOKEN
+      };
     }
-    this.connection = new Ws(this.url, this.protocol, undefined, headers, this.requestOptions, this.clientConfig);
+    this.connection = new Ws(
+      this.url,
+      this.protocol,
+      undefined,
+      headers,
+      this.requestOptions,
+      this.clientConfig
+    );
     this._addSocketListeners();
   }
-}
+};
